86e6e6d185a7ccbf92a083ea85785fb537a1c1f3
RodrigoDLPontes <rodrigo.dl.pontes@gmail.com>
2018-05-19 23:26:14 -0400

    Fixed BBCodeParserTest style and added Javadoc to BBCodeParser

--- src/main/java/codeu/service/BBCodeParser.java
+++ src/main/java/codeu/service/BBCodeParser.java
@@ -2,6 +2,11 @@ package codeu.service;
 
 public class BBCodeParser {
 
+  /**
+   * Parses the input, substituting BBCode tags with their equivalent HTML tags
+   * @param input The text to be parsed
+   * @return The parsed text
+   */
   public static String parse(String input) {
     StringBuilder builder = new StringBuilder(input);
 
