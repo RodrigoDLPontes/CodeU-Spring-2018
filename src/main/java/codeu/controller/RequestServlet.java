bea858aceb9450f7c2b00bbda11065104125cedf
Alexis Mejia <alexisnmejia@gmail.com>
2018-07-09 22:24:48 -0400

    updated RequestServlet

--- src/main/java/codeu/controller/RequestServlet.java
+++ src/main/java/codeu/controller/RequestServlet.java
@@ -1,5 +1,30 @@
 package codeu.controller;
 
-public class RequestServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RequestServlet extends HttpServlet {
+  /**
+   * Set up state for handling admin-related requests. This method is only called when
+   * running in a server, not when running in a test.
+   */
+  @Override
+  public void init() throws ServletException {
+    super.init();
+  }
+
+  /**
+   * Serves the admin page for the logged in user. Can access the page directly from /admin url
+   * or can click through from the homepage (link shown only to admins).
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    request.getRequestDispatcher("/WEB-INF/view/request.jsp").forward(request, response);
+    return;
+  }
 }
