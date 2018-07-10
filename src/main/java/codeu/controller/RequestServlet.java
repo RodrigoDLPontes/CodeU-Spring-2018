11543fbc8b5843b18fc586f629464c9ce0cbd26e
Alexis Mejia <alexisnmejia@gmail.com>
2018-07-09 23:34:31 -0400

    Updated requests page

--- src/main/java/codeu/controller/RequestServlet.java
+++ src/main/java/codeu/controller/RequestServlet.java
@@ -8,18 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class RequestServlet extends HttpServlet {
-  /**
-   * Set up state for handling admin-related requests. This method is only called when
-   * running in a server, not when running in a test.
-   */
+  
   @Override
   public void init() throws ServletException {
     super.init();
   }
 
   /**
-   * Serves the admin page for the logged in user. Can access the page directly from /admin url
-   * or can click through from the homepage (link shown only to admins).
+   * Serves the requests page for the logged in user. Can access the page directly from /request url
    */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
